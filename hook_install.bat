332376bd461b209d43cebafd5b76940a4ff6267d
Pavel Lymarev <x-user@bk.ru>
2017-04-22 19:22:37 +0300

    Updated hook install/uninstall scripts

--- hook_install.bat
+++ hook_install.bat
@@ -1,16 +1,34 @@
 @echo off
+set dllpath="%~dp0%~1\hook.dll"
+
 set _os_bitness=64
 if %PROCESSOR_ARCHITECTURE% == x86 (
 	if not defined PROCESSOR_ARCHITEW6432 set _os_bitness=32
 )
 echo Operating System is %_os_bitness% bit
 if _os_bitness == 32 (
-	set key="HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows"
+	set key=HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows
 ) else (
-	set key="HKLM\Software\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Windows"
+	set key=HKLM\Software\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Windows
 )
 
-reg add %key% /v AppInit_DLLs /d %~dp0%~1\hook.dll /f
-reg add %key% /v LoadAppInit_DLLs /t REG_DWORD /d 1
-reg add %key% /v RequireSignedAppInit_DLLs /t REG_DWORD /d 0
+echo.
+echo This will change following registry values:
+
+echo.
+echo %key%
+echo    AppInit_DLLs to %dllpath%
+echo    LoadAppInit_DLLs to 1
+echo    RequireSignedAppInit_DLLs to 0
+
+echo.
+set /p confirm=Are you sure (Y/[N])? 
+if /i "%confirm%" == "Y" (
+	echo.
+	echo Installing..
+	reg add "%key%" /v AppInit_DLLs /d "%dllpath%" /f
+	reg add "%key%" /v LoadAppInit_DLLs /t REG_DWORD /d 1 /f
+	reg add "%key%" /v RequireSignedAppInit_DLLs /t REG_DWORD /d 0 /f
+)
+
 pause
--- hook_uninstall.bat
+++ hook_uninstall.bat
@@ -5,12 +5,28 @@ if %PROCESSOR_ARCHITECTURE% == x86 (
 )
 echo Operating System is %_os_bitness% bit
 if _os_bitness == 32 (
-	set key="HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows"
+	set key=HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows
 ) else (
-	set key="HKLM\Software\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Windows"
+	set key=HKLM\Software\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Windows
 )
 
-reg add %key% /v AppInit_DLLs /d "" /f
-reg add %key% /v LoadAppInit_DLLs /t REG_DWORD /d 0
-reg add %key% /v RequireSignedAppInit_DLLs /t REG_DWORD /d 1
+echo.
+echo This will change following registry values:
+
+echo.
+echo %key%
+echo    AppInit_DLLs to ""
+echo    LoadAppInit_DLLs to 0
+echo    RequireSignedAppInit_DLLs to 1
+
+echo.
+set /p confirm=Are you sure (Y/[N])? 
+if /i "%confirm%" == "Y" (
+	echo.
+	echo Unistalling..
+	reg add "%key%" /v AppInit_DLLs /d "" /f
+	reg add "%key%" /v LoadAppInit_DLLs /t REG_DWORD /d 0 /f
+	reg add "%key%" /v RequireSignedAppInit_DLLs /t REG_DWORD /d 1 /f
+)
+
 pause
