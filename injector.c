2013541e25b32d1e1f935651d78b22183df42e1d
Pavel Lymarev <x-user@bk.ru>
2017-04-22 15:45:12 +0300

    Little fix

--- injector.c
+++ injector.c
@@ -164,9 +164,11 @@ BOOL injectFunct(DWORD dwProcId) {
 								NULL);
 
 						if (hThread) {
-							result = TRUE;
 							// wait until remote thread finish it's work
 							WaitForSingleObject(hThread, INFINITE);
+
+							result = TRUE;
+							CloseHandle(hThread);
 					}	}
 
 					VirtualFreeEx(hProc, mem, 0, MEM_RELEASE);
