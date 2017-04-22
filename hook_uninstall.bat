4b4b3bb8ba69dfccd9379fa2192b2b12a8f68a3f
Pavel Lymarev <x-user@bk.ru>
2017-04-22 17:55:51 +0300

    Added cmd scripts for global hook installing

---
+++ hook_uninstall.bat
@@ -0,0 +1,16 @@
+@echo off
+set _os_bitness=64
+if %PROCESSOR_ARCHITECTURE% == x86 (
+	if not defined PROCESSOR_ARCHITEW6432 set _os_bitness=32
+)
+echo Operating System is %_os_bitness% bit
+if _os_bitness == 32 (
+	set key="HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows"
+) else (
+	set key="HKLM\Software\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Windows"
+)
+
+reg add %key% /v AppInit_DLLs /d "" /f
+reg add %key% /v LoadAppInit_DLLs /t REG_DWORD /d 0
+reg add %key% /v RequireSignedAppInit_DLLs /t REG_DWORD /d 1
+pause
