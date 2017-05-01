dadc2f8fa83a98be4180659716fd916f37cf0a8e
Pavel Lymarev <x-user@bk.ru>
2017-05-02 01:00:37 +0300

    Little fixes.

--- error.c
+++ error.c
@@ -4,15 +4,15 @@ void printLastError() {
 	LPSTR lpMsgBuf;
 	// get last error code
 	DWORD dwLastError = GetLastError();
-	
+
 	// get message from error code
 	FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | 
 				   FORMAT_MESSAGE_FROM_SYSTEM |
 				   FORMAT_MESSAGE_IGNORE_INSERTS,
 				   NULL, dwLastError,
 				   MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-				   lpMsgBuf, 0, NULL);
-	
+				   (LPSTR) &lpMsgBuf, 0, NULL);
+
 	// print error message
 	printf("Error %d: %s\n", dwLastError, lpMsgBuf);
 }
--- screenshot.bat
+++ screenshot.bat
@@ -1,4 +1,7 @@
 @echo off
 del screenshot.bmp
+screenshot.exe
+echo.
+injector.exe screenshot.exe hook.dll
 injector.exe screenshot.exe hook_new.dll
 pause
